a3f8300022309631994083c29c64380cc17e1ea5
Nathan Peach <144242721+PeachCube@users.noreply.github.com>
2023-09-18 15:30:31 -0300

    Added ground tiles, began writing jump control for the player

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -5,7 +6,8 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class Controller : MonoBehaviour
 {
-    private Rigidbody rb;
+    public float jumpStrength = 1;
+    public Rigidbody rb;
     private float movementX;
     private float movementY;
     public float speed = 0;
@@ -25,4 +27,14 @@ public class Controller : MonoBehaviour
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
         rb.AddForce(movement * speed);
     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce((Vector3.up) * (jumpStrength), ForceMode.Impulse);
+        }
+    }
+
+
+
 }
