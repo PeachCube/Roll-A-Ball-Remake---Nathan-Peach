457178b5aa62faf529465164d7c48733aba3f4fa
Nathan Peach <144242721+PeachCube@users.noreply.github.com>
2023-09-19 12:37:15 -0300

    Tuesday class 2 updates

---
+++ Assets/Rotator.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    void Start()
+    {
+        ;
+    }
+    void Update()
+    {
+        
+    }
+}
