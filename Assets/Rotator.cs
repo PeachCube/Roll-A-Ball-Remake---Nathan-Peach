571692e1fa6c8f487c5f205ccf440380c6f5c20f
PeachCube <nathanpeach500@live.com>
2023-09-19 14:40:09 -0300

    Small experimenting

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
     void Start()
     {
-        ;
+        StartCoroutine(Rotor());
     }
-    void Update()
+    IEnumerator Rotor()
     {
-        
+        transform.Rotate(new Vector3(0, 0, 90), Space.World);
+        yield return new WaitForSeconds(3); 
     }
 }
